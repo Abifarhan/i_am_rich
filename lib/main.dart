572ccc758777c64791b8177322c5afe992e24d26
abiFarhan <abifarhan04@gmail.com>
2021-11-12 10:42:03 +0700

    Learn Scaffold -> trying to tall image from assets local

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                "https://media.istockphoto.com/photos/coco-coconut-half-and-piece-isolated-cocos-white-full-depth-of-field-picture-id1158975559?b=1&k=20&m=1158975559&s=170667a&w=0&h=4XYQ2BX1EB1S4NPSJ0kaZ0ZRd_qeMDVGnnB8QbJPFyY="),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
