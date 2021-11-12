48976880b83d5aecec6d7c688bc395a5fd56e76b
abiFarhan <abifarhan04@gmail.com>
2021-11-12 10:23:13 +0700

    Learn Scaffold -> Prepare implement adding image from local source

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,12 @@ void main() {
           title: Text("I Am Rich"),
           backgroundColor: Colors.blueGrey[1000],
         ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                "https://media.istockphoto.com/photos/coco-coconut-half-and-piece-isolated-cocos-white-full-depth-of-field-picture-id1158975559?b=1&k=20&m=1158975559&s=170667a&w=0&h=4XYQ2BX1EB1S4NPSJ0kaZ0ZRd_qeMDVGnnB8QbJPFyY="),
+          ),
+        ),
       ),
     ),
   );
