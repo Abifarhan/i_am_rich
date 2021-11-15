6f447acc5568b19cc2b328bff45016fdbd94ad9e
abiFarhan <abifarhan04@gmail.com>
2021-11-15 11:47:14 +0700

    Design Layout -> exploration Container

--- my_card_name/lib/main.dart
+++ my_card_name/lib/main.dart
@@ -6,15 +6,15 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
+          child: Row(
             // mainAxisSize: MainAxisSize.min,
             //     verticalDirection: VerticalDirection.down,
             //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
-                height: 100.0,
-                width: double.infinity,
+                // height: 100.0,
+                // width: double.infinity,
 // margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
 // margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
 // margin: EdgeInsets.only(left: 30.0),
@@ -27,15 +27,15 @@ void main() {
 
               ),
               Container(
-                width: double.infinity,
-                height: 100.0,
+                // width: double.infinity,
+                // height: 100.0,
                 // margin: EdgeInsets.only(top: 20.0),
                 color: Colors.blue,
                 child: Text('Hello this is container 2'),
               ),
               Container(
-                width: double.infinity,
-                height: 100.0,
+                // width: double.infinity,
+                // height: 100.0,
                 // margin: EdgeInsets.only(top: 20.0),
                 color: Colors.redAccent,
                 child: Text('Hello this is container 3'),
