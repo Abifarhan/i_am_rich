1e993a29dea643c5910b0af0f7c9b15f7eb83f53
abiFarhan <abifarhan04@gmail.com>
2021-11-15 10:20:28 +0700

    trying to change background

---
+++ my_card_name/android/app/src/main/kotlin/com/example/my_card_name/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_card_name
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ my_card_name/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(),
+      ),
+    ),
+  );
+}
+
+
+// class  extends StatelessWidget {
+//   const ({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+
+
+
