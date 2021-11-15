9171de92f6db64d7be971ec578ea454d85c55b7b
abiFarhan <abifarhan04@gmail.com>
2021-11-15 11:36:37 +0700

    Design Layout -> Implementing Container variation part 3

--- my_card_name/lib/main.dart
+++ my_card_name/lib/main.dart
@@ -6,43 +6,39 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          // mainAxisSize: MainAxisSize.min,
-          //     verticalDirection: VerticalDirection.down,
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              width: 100.0,
+          child: Column(
+            // mainAxisSize: MainAxisSize.min,
+            //     verticalDirection: VerticalDirection.down,
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: double.infinity,
 // margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
 // margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
 // margin: EdgeInsets.only(left: 30.0),
 // padding: EdgeInsets.all(20.0),
-              color: Colors.white,
-              child: const Text('Container 1'),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              margin: EdgeInsets.only(top: 20.0),
-              color: Colors.blue,
-              child: Text('Hello this is container 2'),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              margin: EdgeInsets.only(top: 20.0),
-              color: Colors.redAccent,
-              child: Text('Hello this is container 3'),
-            ),
-            Container(
-              width: double.infinity,
-              height: 10.0,
-
-            )
-          ],
-        )),
+                color: Colors.white,
+                child: const Text('Container 1'),
+              ),
+              Container(
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.only(top: 20.0),
+                color: Colors.blue,
+                child: Text('Hello this is container 2'),
+              ),
+              Container(
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.only(top: 20.0),
+                color: Colors.redAccent,
+                child: Text('Hello this is container 3'),
+              )
+            ],
+          ),
+        ),
       ),
     ),
   );
